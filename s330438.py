365b74c4e55f3e0895d5dcade00a5b2302ea22d0
GDennis01 <dennis.gobbi001@gmail.com>
2025-01-22 14:21:49 +0100

    Updated formule

--- s330438.py
+++ s330438.py
@@ -4,29 +4,24 @@ import numpy as np
 def f1(x: np.ndarray) -> np.ndarray: 
     return np.sin(x[0])
 
-
 def f2(x: np.ndarray) -> np.ndarray: 
-    return 0
-
+    return (np.power(7.94684493268025, x[1]) + (x[0] + x[2]))
 
 def f3(x: np.ndarray) -> np.ndarray: 
-    return 0
-
+    return (((((x[2] * -3.5011335892847057) - -4.214556756192762) + (-(x[1]) * np.square(x[1]))) - ((9.873283610833018e-07 - x[0]) * (x[0] + x[0]))) - 0.2143513886781467)
 
 def f4(x: np.ndarray) -> np.ndarray: 
     return (np.cos(x[1]) * (((0.23024387669493285 * (9.49261846513043 / np.cos(x[1]))) + ((1.0918467832820689 / np.cos(x[1])) - -6.9747658112794575)) - ((0.009416198631637944 * (9.671215081247784 / np.cos(x[1]))) * (x[0] - (0.2702890211737155 * np.cos(x[1]))))))
 
 def f5(x: np.ndarray) -> np.ndarray: 
-    return ((x[0] / -1.1029017263054506) / np.cosh(((9.856509033734056 * -2.594225902466807) + x[1])))
-
+    return ((x[0] / -1.1029017263054506) / np.cosh((-25.57001104321097 + x[1])))
 
 def f6(x: np.ndarray) -> np.ndarray: 
     return (((x[1] / 0.6163310509098459) / 0.9575008504359739) - (x[0] / 1.4398424197751116))
 
-
 def f7(x: np.ndarray) -> np.ndarray: 
     return  np.exp(np.square((0.19412533614260724 * (((x[1] * x[0]) * (1.2981600350564866 - np.sin(((0.7048732443752681 - (x[1] * x[0])) * -0.12545609726467288)))) + ((((0.9401730004516422 / np.power(0.9190484173090112, np.remainder(x[0], 0.17908034227130276))) * np.power(np.power(1.0075774564033242, np.remainder(x[0], -0.5453290513341242)), ((x[1] + -0.5453290513341242) * x[0]))) - np.absolute(np.arctan(np.arctan((x[0] - x[1]))))) * ((1.7743339625324417 * (0.5437591283769042 + np.power(1.0272695518587667, np.remainder(-1.646242327114337, x[1])))) + np.exp((np.power(0.9704417384263868, np.remainder(x[0], 0.9659013507711744)) + np.power(0.9561606006321369, (x[0] - x[1]))))))))))
 
-
 def f8(x: np.ndarray) -> np.ndarray:
     return np.minimum(((((np.sinh(x[5]) * np.maximum(9.280652473985764, ((x[0] / x[0]) - (x[1] - 0.4059194895949467)))) * np.maximum(np.maximum(np.minimum(np.cosh(x[5]), 18.44238954070858), -1.9818022307528853), (np.minimum(17.6972179719256, np.square(x[5])) + np.minimum(np.square(x[5]), np.absolute(x[5]))))) + np.minimum((np.minimum(np.sinh(x[4]), 17.6972179719256) * 36.72752662020359), (np.minimum(36.33547306342092, (np.sinh(x[5]) + np.cosh(x[5]))) + ((np.sinh(x[4]) * -4.070986083664829) * 8.977972163360176)))) - np.minimum(np.maximum(np.remainder((np.maximum(0.4059194895949467, (x[3] - 0.7506252992942812)) + (np.remainder(x[4], -4.109412935321963) + 3.025150828938454)), np.minimum(np.minimum(np.sinh(x[4]), 2.6207832272206506), (np.minimum(-9.174199129628121, x[2]) + -17.86111661118901))), (np.maximum((np.remainder(x[4], -4.109412935321963) * 97.8102957631067), -82.98976582413408) + ((np.maximum(-9.174199129628121, x[3]) * 97.8102957631067) / ((5.980665508500083 / x[3]) * 1.0638269899324038)))), np.remainder(np.maximum((100.01994107697637 / (x[3] - 2.1332481808545545)), -0.09228238684307954), (np.minimum(36.33547306342092, (-4.109412935321963 * np.maximum(-9.174199129628121, x[3]))) * 17.361335511767116)))), ((((np.minimum((np.maximum(-9.174199129628121, x[3]) + np.sinh(x[5])), (np.cosh(x[5]) + -1.4254744449215995)) * (np.minimum(9.720508873329518, np.square(x[5])) + np.minimum(7.990198483701521, np.absolute(x[5])))) + (np.maximum((44.82781934259213 / np.absolute(x[5])), (np.absolute(x[5]) + 6.685971936727686)) * np.maximum((np.sinh(x[5]) + np.cos(x[5])), 1.0072334326500667))) * 8.680667755883558) - np.minimum(np.square(np.maximum((-1.4904791954484304 * np.sinh(x[5])), (np.minimum(17.6972179719256, np.cosh(x[5])) + np.minimum(np.sinh(x[4]), 23.467486856065708)))), np.square(np.minimum(np.minimum(-5.420044949989919, np.minimum(np.square(x[5]), np.square(x[5]))), (-33.91491388432617 + np.maximum(-4.067873225649736, (x[3] - 3.025150828938454))))))))
+
